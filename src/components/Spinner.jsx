b773a8c9ea2720b0665fba98b9692eb5776309a4
Xavier <luis.ochoa02@epn.edu.ec>
2026-05-14 00:07:28 -0500

    Delete src directory

--- src/components/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-export default function Spinner({ size = 'md' }) {
-  const s = { sm: 'h-4 w-4', md: 'h-8 w-8', lg: 'h-12 w-12' }[size]
-  return (
-    <div className="flex justify-center items-center py-8">
-      <div className={`${s} animate-spin rounded-full border-4 border-gray-200 border-t-primary-700`} />
-    </div>
-  )
-}
